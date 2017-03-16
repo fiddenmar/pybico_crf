16663ebc9d0882d0f16fbfa790fd0d9958039b58
Андрей <druna.94@mail.ru>
2017-03-16 19:46:50 +0300

    Добавлена возможность обработки массива строк в predict.py (#12)
    predict changed, arrays support added

--- predict.py
+++ predict.py
@@ -6,11 +6,19 @@ from features import word2features, sent2features, sent2labels, sent2tokens
 
 crf = joblib.load('model.pkl')
 
-def predict(sentance):
-	target = [sent2features(s) for s in [sentance.split()]]
+def predict_one(sentence):
+	target = [sent2features(s) for s in [sentence.split()]]
 	return crf.predict(target)
+
+def predict(sentence_array):
+	results=[]
+
+	for sentence in sentence_array:
+		result=predict_one(sentence)
+		results.append(result)
+	return results
 
 if __name__ == "__main__":
 	inp = input("Input string: ")
-	pred = predict(inp)
+	pred = predict_one(inp)
 	print(list(zip(inp.split(), *pred)))
